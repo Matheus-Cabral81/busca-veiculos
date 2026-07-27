b2053479c213687828a66301f66f39e77d60d14b
Matheus-Cabral81 <matheuscabral2001@outlook.com>
2026-07-26 22:25:50 -0300

    Inicio projeto

---
+++ backend/src/main/java/com/vpo/busca_veiculos/BuscaVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.vpo.busca_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscaVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscaVeiculosApplication.class, args);
+	}
+
+}
